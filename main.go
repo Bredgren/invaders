3c1c30cbcb4f2f70067f7a08b4fe3d4074b03780
Bredgren <brandon.edgren@gmail.com>
2017-08-04 20:08:19 -0700

    Add player image and draw it to screen.

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	_ "image/png"
+	"log"
 
+	"github.com/Bredgren/geo"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
@@ -12,14 +15,29 @@ const (
 	Height = 240
 )
 
+var (
+	playerImg *ebiten.Image
+)
+
 func update(screen *ebiten.Image) error {
+	pos := geo.VecXY(Width/2, Height/2)
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Reset()
+	opts.GeoM.Translate(pos.XY())
+	screen.DrawImage(playerImg, opts)
+
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.CurrentFPS()))
 	return nil
 }
 
 func main() {
+	var err error
+	playerImg, _, err = ebitenutil.NewImageFromFile("img/player.png", ebiten.FilterNearest)
+	if err != nil {
+		log.Fatal("open player file:", err)
+	}
 	if err := ebiten.Run(update, Width, Height, 2, "Invaders"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println("bye")
 }
