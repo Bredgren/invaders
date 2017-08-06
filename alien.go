a8d533d4a29081d5d82adeaeec3569f5185ff46f
Bredgren <brandon.edgren@gmail.com>
2017-08-06 14:06:41 -0700

    Create and draw all the aliens.

--- alien.go
+++ alien.go
@@ -12,10 +12,16 @@ var (
 	tempo = 1.0
 )
 
+type Alien struct {
+	Rect geo.Rect
+	kind int
+}
+
 type Aliens struct {
 	Bounds   geo.Rect
 	AlienImg [3][2]*ebiten.Image
 	Opts     *ebiten.DrawImageOptions
+	Aliens   [11 * 5]Alien
 	counter  float64
 }
 
@@ -27,7 +33,40 @@ func (a *Aliens) init() {
 	a.AlienImg[2][0] = openImg("img/alien3_00.png")
 	a.AlienImg[2][1] = openImg("img/alien3_01.png")
 	a.Opts = &ebiten.DrawImageOptions{}
-	a.Bounds = geo.RectXYWH(50, 100, 100, 50)
+	a.Bounds = geo.RectXYWH(40, 64, 0, 0)
+
+	xSpacing, ySpacing := 16.0, 16.0
+	x, y := a.Bounds.TopLeft()
+
+	for row := 0; row < 5; row++ {
+		kind := alienKindForRow(row)
+		offset := float64(kind)
+		wi, hi := a.AlienImg[kind][0].Size()
+		w, h := float64(wi), float64(hi)
+		for col := 0; col < 11; col++ {
+			a.Aliens[11*row+col] = Alien{
+				Rect: geo.RectXYWH(x+offset, y, w, h),
+				kind: kind,
+			}
+			x += xSpacing
+		}
+		x = a.Bounds.Left()
+		y += ySpacing
+	}
+
+	for _, alien := range a.Aliens {
+		a.Bounds.Union(alien.Rect)
+	}
+}
+
+func alienKindForRow(row int) int {
+	switch row {
+	case 0:
+		return 2
+	case 1, 2:
+		return 1
+	}
+	return 0
 }
 
 func (a *Aliens) update(dt time.Duration) {
@@ -35,13 +74,10 @@ func (a *Aliens) update(dt time.Duration) {
 }
 
 func (a *Aliens) draw(dst *ebiten.Image) {
-	x, y := a.Bounds.TopLeft()
-	i := int(a.counter) % 2
-	a.Opts.GeoM.Reset()
-	a.Opts.GeoM.Translate(x, y)
-	dst.DrawImage(a.AlienImg[0][i], a.Opts)
-	a.Opts.GeoM.Translate(20, 0)
-	dst.DrawImage(a.AlienImg[1][i], a.Opts)
-	a.Opts.GeoM.Translate(20, 0)
-	dst.DrawImage(a.AlienImg[2][i], a.Opts)
+	idx := int(a.counter) % 2
+	for _, alien := range a.Aliens {
+		a.Opts.GeoM.Reset()
+		a.Opts.GeoM.Translate(alien.Rect.TopLeft())
+		dst.DrawImage(a.AlienImg[alien.kind][idx], a.Opts)
+	}
 }
